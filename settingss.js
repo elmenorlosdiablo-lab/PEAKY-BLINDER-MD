88f2709c5358ab205a056951d0a5a94dd938b5c3
elmenorlosdiablo-lab <elmenorlosdiablo@gmail.com>
2025-11-03 17:09:47 -0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~QQh1iJRY#DbCKJ_uk0mvO2Hqm3A1Yiu6IvaDR64cGcwdQdq6JW3E",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
